24f90a459a779c009843ebc9b563f2aa14c05801
Ishan-Pradhan <pradhan.ishan70@gmail.com>
2024-05-10 22:55:17 +0545

    utils codes

--- src/index.js
+++ src/index.js
@@ -2,9 +2,21 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
+import express from "express";
+
+const app = express();
+
 dotenv.config({ path: "./env" });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is running at port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Mongo db connection failed !!! ", err);
+  });
 
 /*
 import express from "express";
